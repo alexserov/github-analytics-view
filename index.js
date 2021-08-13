a2cd8dd73fde752d675dd62e6219facd6fb97705
Alexey Serov <alexey.serov@devexpress.com>
2021-08-13 13:46:05 +0300

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-$(() => {
-    let store = new DevExpress.data.CustomStore({
+function createStore(endpoint) {
+    return new DevExpress.data.CustomStore({
         load: async options => {
-            let url = 'https://serov-github-analytics.herokuapp.com/runs';
+            let url = `https://serov-github-analytics.herokuapp.com/${endpoint}`;
             if (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-                url = 'http://localhost:3000/runs';
+                url = `http://localhost:3000/${endpoint}`;
 
             return await new Promise(resolve => {
                 $.ajax({
@@ -15,42 +15,70 @@ $(() => {
             });
         }
     });
-    $(function () {
-        $("#rangeSelector").dxRangeSelector({
-            dataSource: new DevExpress.data.DataSource({
-                paginate: false,
-                store: store
-            }),
-            margin: {
-                top: 50
+}
+$(() => {
+    let store = createStore('runs');
+    $("#rangeSelector").dxRangeSelector({
+        dataSource: new DevExpress.data.DataSource({
+            paginate: false,
+            store: store,
+            filter: 'skip'
+        }),
+        size: {
+            height: 300
+        },
+        title: 'Pending time/Run time',
+        chart: {
+            commonSeriesSettings: {
+                type: "steparea",
+                argumentField: "date"
             },
-            size: {
-                height: 300
+            series: [
+                { valueField: "pending", color: "red" },
+                { valueField: "duration", color: "green" }
+            ]
+        },
+        scale: {
+            minorTickInterval: "minute",
+            tickInterval: "hour",
+            valueType: "datetime"
+        },
+        sliderMarker: {
+            format: "shortDateShortTime"
+        },
+        onValueChanged: x => {
+            var dataSource = $("#dataGrid").dxDataGrid("getDataSource");
+            dataSource.filter(x.value);
+            dataSource.load();
+        }
+    });
+    $("#rangeSelectorDates").dxRangeSelector({
+        dataSource: new DevExpress.data.DataSource({
+            paginate: false,
+            store: createStore('dates')
+        }),
+        title: 'Total action runs',
+        chart: {
+            commonSeriesSettings: {
+                type: "steparea",
+                argumentField: "date"
             },
-            chart: {
-                commonSeriesSettings: {
-                    type: "steparea",
-                    argumentField: "date"
-                },
-                series: [
-                    { valueField: "pending", color: "red" },
-                    { valueField: "duration", color: "green" }
-                ]
-            },
-            scale: {
-                minorTickInterval: "minute",
-                tickInterval: "hour",
-                valueType: "datetime"
-            },
-            sliderMarker: {
-                format: "shortDateShortTime"
-            },
-            onValueChanged: x => {
-                var dataSource = $("#dataGrid").dxDataGrid("getDataSource");
-                dataSource.filter(x.value);
-                dataSource.load();
-            }
-        });
+            series: [
+                { valueField: "count", color: "red" },
+            ]
+        },
+        scale: {
+            tickInterval: "hour",
+            valueType: "datetime"
+        },
+        sliderMarker: {
+            format: "shortDateShortTime"
+        },
+        onValueChanged: x => {
+            var dataSource = $("#rangeSelector").dxRangeSelector("getDataSource");
+            dataSource.filter(x.value);
+            dataSource.load();
+        }
     });
     $('#dataGrid').dxDataGrid({
         dataSource: new DevExpress.data.DataSource({
@@ -59,12 +87,14 @@ $(() => {
         }),
         remoteOperations: {
             filtering: true,
-            sorting: true,
-            grouping: true
+            sorting: false,
+            grouping: false
         },
         columns: [
+            { dataField: 'repository' },
             { dataField: 'name' },
             { dataField: 'date' },
+            { dataField: 'status' },
             { dataField: 'duration' },
             { dataField: 'pending' },
         ]
