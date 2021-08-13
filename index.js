d965a229619bdfa01acb0ef8f79f2faa07ceced7
Alexey Serov <alexey.serov@devexpress.com>
2021-08-13 16:09:24 +0300

    update

--- index.js
+++ index.js
@@ -1,102 +1,133 @@
 function createStore(endpoint) {
     return new DevExpress.data.CustomStore({
-        load: async options => {
+        load: (options) => {
             let url = `https://serov-github-analytics.herokuapp.com/${endpoint}`;
-            if (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-                url = `http://localhost:3000/${endpoint}`;
+            // eslint-disable-next-line no-restricted-globals
+            if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') { url = `http://localhost:3000/${endpoint}`; }
 
-            return await new Promise(resolve => {
+            return new Promise((resolve) => {
                 $.ajax({
                     type: 'GET',
-                    url: url,
+                    url,
                     data: options,
-                    success: response => resolve(response)
+                    success: (response) => resolve(response),
                 });
             });
-        }
+        },
     });
 }
 $(() => {
-    let store = createStore('runs');
-    $("#rangeSelector").dxRangeSelector({
+    const store = createStore('runs');
+    $('#rangeSelector').dxRangeSelector({
         dataSource: new DevExpress.data.DataSource({
             paginate: false,
-            store: store,
-            filter: 'skip'
+            store,
+            filter: 'skip',
         }),
         size: {
-            height: 300
+            height: 300,
         },
         title: 'Pending time/Run time',
         chart: {
             commonSeriesSettings: {
-                type: "steparea",
-                argumentField: "date"
+                type: 'steparea',
+                argumentField: 'date',
             },
             series: [
-                { valueField: "pending", color: "red" },
-                { valueField: "duration", color: "green" }
-            ]
+                { valueField: 'pending', color: 'red' },
+                { valueField: 'duration', color: 'green' },
+            ],
         },
         scale: {
-            minorTickInterval: "minute",
-            tickInterval: "hour",
-            valueType: "datetime"
+            minorTickInterval: 'minute',
+            tickInterval: 'hour',
+            valueType: 'datetime',
         },
         sliderMarker: {
-            format: "shortDateShortTime"
+            format: 'shortDateShortTime',
         },
-        onValueChanged: x => {
-            var dataSource = $("#dataGrid").dxDataGrid("getDataSource");
+        onValueChanged: (x) => {
+            const dataSource = $('#dataGrid').dxDataGrid('getDataSource');
             dataSource.filter(x.value);
             dataSource.load();
-        }
+        },
     });
-    $("#rangeSelectorDates").dxRangeSelector({
+    $('#rangeSelectorDates').dxRangeSelector({
         dataSource: new DevExpress.data.DataSource({
             paginate: false,
-            store: createStore('dates')
+            store: createStore('dates'),
         }),
         title: 'Total action runs',
         chart: {
             commonSeriesSettings: {
-                type: "steparea",
-                argumentField: "date"
+                type: 'steparea',
+                argumentField: 'date',
             },
             series: [
-                { valueField: "count", color: "red" },
-            ]
+                { valueField: 'count', color: 'red' },
+            ],
         },
         scale: {
-            tickInterval: "hour",
-            valueType: "datetime"
+            tickInterval: 'hour',
+            valueType: 'datetime',
         },
         sliderMarker: {
-            format: "shortDateShortTime"
+            format: 'shortDateShortTime',
         },
-        onValueChanged: x => {
-            var dataSource = $("#rangeSelector").dxRangeSelector("getDataSource");
+        onValueChanged: (x) => {
+            const dataSource = $('#rangeSelector').dxRangeSelector('getDataSource');
             dataSource.filter(x.value);
             dataSource.load();
-        }
+        },
     });
+    const formatMiliseconds = (cell) => {
+        if (cell.value) {
+            const valueNum = Math.ceil((+cell.value) / 1000);
+            const minutes = Math.floor(valueNum / 60);
+            const seconds = valueNum - minutes * 60;
+            return `${minutes}m ${seconds.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })}s`;
+        }
+        return undefined;
+    };
     $('#dataGrid').dxDataGrid({
         dataSource: new DevExpress.data.DataSource({
-            store: store,
-            filter: 'skip'
+            store,
+            filter: 'skip',
         }),
+        groupPanel: {
+            visible: true,
+        },
         remoteOperations: {
             filtering: true,
             sorting: false,
-            grouping: false
+            grouping: false,
         },
         columns: [
             { dataField: 'repository' },
             { dataField: 'name' },
             { dataField: 'date' },
+            {
+                caption: 'URL',
+                cellTemplate: (container, options) => {
+                    $('<a>')
+                        .text('Link')
+                        .attr({
+                            href: options.data.url,
+                            target: '_blank',
+                        })
+                        .appendTo(container);
+                },
+            },
             { dataField: 'status' },
-            { dataField: 'duration' },
-            { dataField: 'pending' },
-        ]
+            { dataField: 'conclusion' },
+            {
+                dataField: 'duration',
+                customizeText: formatMiliseconds,
+            },
+            {
+                dataField: 'pending',
+                customizeText: formatMiliseconds,
+            },
+        ],
     });
 });
